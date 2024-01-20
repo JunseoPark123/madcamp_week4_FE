b053d91a8ba03e216738c8e203bb0d7adf21ebd1
JunseoPark123 <pjunseo7965@gmail.com>
2024-01-21 02:20:43 +0900

    binding map view

--- app/src/main/java/com/example/madcamp_week4_fe/HeartFragment.kt
+++ app/src/main/java/com/example/madcamp_week4_fe/HeartFragment.kt
@@ -5,11 +5,53 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.madcamp_week4_fe.databinding.FragmentHeartBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
 
-class HeartFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_heart, container, false)
+class HeartFragment : Fragment(), OnMapReadyCallback {
+    private var _binding: FragmentHeartBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var map: GoogleMap
 
-        return view
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = FragmentHeartBinding.inflate(inflater, container, false)
+        binding.mapView.onCreate(savedInstanceState)
+        binding.mapView.getMapAsync(this)
+        return binding.root
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        val southKorea = LatLng(36.0, 128.0) // 남한의 대략적인 중심 좌표
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(southKorea, 6.0f)) // 지도 줌 레벨 설정
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.mapView.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.mapView.onDestroy()
+        _binding = null
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapView.onLowMemory()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        binding.mapView.onSaveInstanceState(outState)
     }
 }
